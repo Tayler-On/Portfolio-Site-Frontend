3e713cabdb9b97419f615b7a2f9061bb50dc2bda
Tayler-On <TaylerOn@umich.edu>
2021-03-31 21:59:16 -0400

    massive update too styling and added more content

--- src/app/final-project/final-project.component.ts
+++ src/app/final-project/final-project.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from '../portfolio.service';
 
 @Component({
   selector: 'app-final-project',
   templateUrl: './final-project.component.html',
-  styleUrls: ['./final-project.component.css']
+  styleUrls: ['./final-project.component.css'],
 })
 export class FinalProjectComponent implements OnInit {
-
-  constructor() { }
+  projects: any = [];
+  constructor(private portfolioService: PortfolioService) {}
 
   ngOnInit(): void {
+    this.getAndSetProject();
   }
 
+  getAndSetProject = () => {
+    this.portfolioService.getProject().subscribe((response: any) => {
+      for (let i = 0; i < response.length; i++) {
+        if (response[i].title === 'Final Project: Get to the Gig') {
+          this.projects.push(response[i]);
+        }
+      }
+    });
+  };
 }
--- src/app/interfaces/projectpage.ts
+++ src/app/interfaces/projectpage.ts
@@ -6,18 +6,8 @@ export interface Projectpage {
   summary: string;
   video: string;
   feat1: string;
-  img1: string;
-  exp1: string;
   feat2: string;
-  img2: string;
-  exp2: string;
   feat3: string;
-  img3: string;
-  exp3: string;
   feat4: string;
-  img4: string;
-  exp4: string;
-  feat5: string;
-  img5: string;
-  exp5: string;
+  logo: string;
 }
